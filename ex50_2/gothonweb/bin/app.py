f60e84b89f8ecb7992db896a7da624b041a9d778
LittleSean <seanseany2014@gmail.com>
2014-11-10 00:47:59 +0800

    ex50_2

---
+++ ex50_2/gothonweb/bin/app.py
@@ -0,0 +1,21 @@
+import web
+
+urls = (
+  '/hello', 'Index'
+)
+
+app = web.application(urls, globals())
+
+render = web.template.render('F:/Dropbox/Python Exercise/Python Exercise/ex50_2/gothonweb/templates', base="layout")
+
+class Index(object):
+    def GET(self):
+        return render.hello_form()
+
+    def POST(self):
+        form = web.input(name="Nobody", greet="Hello")
+        greeting = "%s, %s" % (form.greet, form.name)
+        return render.index(greeting = greeting)
+
+if __name__ == "__main__":
+    app.run()
